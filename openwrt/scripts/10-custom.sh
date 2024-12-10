01b28186232bfa717d00658e8a547c55bd5156be
Masaaki <147675004+yndzm@users.noreply.github.com>
2024-12-11 05:20:33 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 # 自定义脚本
+#Linkease
+git clone --depth=1 --single-branch https://github.com/linkease/istore.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
